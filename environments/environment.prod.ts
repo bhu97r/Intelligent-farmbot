b690088e1cd60d807ff052ba98a2fb2c4ccba50d
bhu97r <92118068+bhu97r@users.noreply.github.com>
2021-11-03 11:57:33 +0530

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyD-65MKCIIIRe-qSO0zPXv0BBf7reJ58cY",
+  authDomain: "chatbot-cpkr.firebaseapp.com",
+  projectId: "chatbot-cpkr",
+  storageBucket: "chatbot-cpkr.appspot.com",
+  messagingSenderId: "863366315903",
+  appId: "1:863366315903:web:fbff92cf7e85ee9b4ca44a",
+  measurementId: "G-RXS1N4HPV9"
+  }
+  
+};
---
+++ environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyD-65MKCIIIRe-qSO0zPXv0BBf7reJ58cY",
+  authDomain: "chatbot-cpkr.firebaseapp.com",
+  projectId: "chatbot-cpkr",
+  storageBucket: "chatbot-cpkr.appspot.com",
+  messagingSenderId: "863366315903",
+  appId: "1:863366315903:web:fbff92cf7e85ee9b4ca44a",
+  measurementId: "G-RXS1N4HPV9"
+    
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
